dedecec7e8ec762d737bd98b39c1b9c0b9ed2380
ailsanajwa <149179618+ailsanajwa@users.noreply.github.com>
2025-03-06 12:27:48 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,23 @@
+public class Data {
+    public static void main(String[] args) {
+        // Data pribadi
+        String nim = "A11.2023.14860"; 
+        String nama = "Ailsa Najwa Insyira Putri"; 
+        String alamat = "Jl Kauman Raya"; 
+        String kota = "Semarang"; 
+		int kodePos = 50117; 
+        String telp = "+628567549699"; 
+        String hp = "+628567549699"; 
+        String email = "ailsanajwa240@gmail.com"; 
+
+        // Menampilkan data
+        System.out.println("Nim     	: "+nim);
+        System.out.println("Nama    	: "+nama);
+        System.out.println("Alamat  	: "+alamat);
+        System.out.println("Kota    	: "+kota);
+        System.out.println("Kode Pos	: "+kodePos);
+        System.out.println("Telp.   	: "+telp);
+        System.out.println("Hp      	: "+hp);
+        System.out.println("Email   	: "+email);
+    }
+}
